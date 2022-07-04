19fbca3993bcd84cb44ee336167fdc2bb482f782
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-04 11:54:56 -0300

    feat: Criando um hook de busca em APIs com swr que tem um sistema de cache e revalidacao

---
+++ src/hooks/useFetcher.jsx
@@ -0,0 +1,18 @@
+import useSWR from 'swr';
+import api from '../services/api';
+
+const useFetcher = (url) => {
+  const { data, error } = useSWR(url, async (urlAPI) => {
+    const response = await api.get(urlAPI);
+
+    return response.data;
+  });
+
+  return {
+    isLoading: !data && !error,
+    data,
+    error,
+  };
+};
+
+export default useFetcher;
