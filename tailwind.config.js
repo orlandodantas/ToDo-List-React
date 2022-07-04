0b9b925d38090a57409c5b77283a224601078bd2
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-04 11:39:22 -0300

    chore: Criando uma backgroundColor customizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'white-rgba': 'rgba(255, 255, 255, 0.2)',
+      },
+    },
   },
   plugins: [],
 };
