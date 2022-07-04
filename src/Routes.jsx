4fb92e48c55242fc30f3e3befe1efd2bdd4c746f
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-04 11:52:46 -0300

    feat: Implementando as rotas publicas e privadas.

---
+++ src/Routes.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Tasks from './pages/Tasks';
+
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({ children, redirectTo }) =>
+  isAuthenticated() ? children : <Navigate to={redirectTo} />;
+
+PrivateRoute.propTypes = {
+  children: PropTypes.element.isRequired,
+  redirectTo: PropTypes.string.isRequired,
+};
+
+const RoutesPage = () => (
+  <Routes>
+    <Route path="/" element={<Login />} />
+    <Route
+      path="tasks"
+      element={
+        <PrivateRoute redirectTo="/">
+          <Tasks />
+        </PrivateRoute>
+      }
+    />
+  </Routes>
+);
+
+export default RoutesPage;
+
+// Credits: https://www.youtube.com/watch?v=x7rL7rPfg5w
