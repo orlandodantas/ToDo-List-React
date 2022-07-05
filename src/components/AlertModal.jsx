482049888b3e119322aa47ed5f32408daed53ba7
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-05 09:49:01 -0300

    refactor: Ajustando para definir a cor de background dinamicamente

--- src/components/AlertModal.jsx
+++ src/components/AlertModal.jsx
@@ -2,27 +2,27 @@ import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 
-const customStyles = {
-  content: {
-    top: '10%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'red',
-    width: '95%',
-  },
-  overlay: {
-    backgroundColor: 'rgba(25, 0, 255, 0.75)',
-  },
-};
-
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement('#root');
 
-const AlertModal = ({ execCloseModal, title, message }) => {
+const AlertModal = ({ execCloseModal, title, message, backgroundColor }) => {
   const [modalIsOpen, setIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: '10%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor,
+      width: '95%',
+    },
+    overlay: {
+      backgroundColor: 'rgba(25, 0, 255, 0.75)',
+    },
+  };
 
   const openModal = () => {
     setIsOpen(true);
@@ -55,10 +55,12 @@ AlertModal.propTypes = {
   execCloseModal: PropTypes.func.isRequired,
   message: PropTypes.string.isRequired,
   title: PropTypes.string,
+  backgroundColor: PropTypes.string,
 };
 
 AlertModal.defaultProps = {
   title: 'Error',
+  backgroundColor: 'red',
 };
 
 export default AlertModal;
